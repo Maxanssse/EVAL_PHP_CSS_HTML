aed1029db26f8412e8c0e7bd42ba2ff3b03877ce
Maxanssse <91048492+Maxanssse@users.noreply.github.com>
2024-05-22 14:05:10 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TODO List</title>
+    <link rel="stylesheet" href="main.css">
+    <script src="main.php"></script>
+</head>
+<body>
+    <main>
+        <form action="traitement.php"></form>
+        <h1 class="title">Ma TODOLIST</h1>
+
+        <input type="submit" value="Ajouter">
+    </main>
+</body>
+</html>
